1c52b116cb1c0bc2e65d5429dd0b10ba64e26cd7
xenerizes <krokodilarseniy@gmail.com>
2017-04-04 18:27:20 +0300

    Added logging; added udf option

--- model/model.py
+++ model/model.py
@@ -1,5 +1,10 @@
 from agent.agent import Agent, Handler
 import agent.udf_pb2 as udf_pb2
+import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(name)s: %(message)s')
+logger = logging.getLogger()
 
 
 class Model(object):
@@ -7,19 +12,35 @@ class Model(object):
 
 
 class MessageHandler(Handler):
-    def __init__(self, agent):
+    def __init__(self, agent, model):
         self._agent = agent
+        self._model = model
+        self._predict = 0
+        self._period = 0
 
     def info(self):
         response = udf_pb2.Response()
         response.info.wants = udf_pb2.BATCH
         response.info.provides = udf_pb2.BATCH
+        response.info.options['predict'].valueTypes.append(udf_pb2.INT)
 
         return response
 
     def init(self, init_req):
         response = udf_pb2.Response()
-        response.init.success = True
+        succ = True
+        msg = ''
+
+        for opt in init_req.options:
+            if opt.name == 'predict':
+                self._predict = opt.values[0].intValue
+
+        if self._predict < 1:
+            succ = False
+            msg += ' must supply number of values to be predicted > 0'
+
+        response.init.success = succ
+        response.init.error = msg
 
         return response
 
@@ -53,7 +74,6 @@ class Accepter(object):
         a = Agent(conn, conn)
         h = MessageHandler(a)
         a.handler = h
-
         logger.info("Starting Agent for connection %d", self._count)
         a.start()
         a.wait()
